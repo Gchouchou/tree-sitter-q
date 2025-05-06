da943712ac7c51192ed439c3f619e67b24fec935
Justin <jusytinyu@gmail.com>
2025-05-06 19:19:19 -0400

    Bump to version 0.2.9

--- src/parser.c
+++ src/parser.c
@@ -94092,7 +94092,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_q(void) {
     .metadata = {
       .major_version = 0,
       .minor_version = 2,
-      .patch_version = 8,
+      .patch_version = 9,
     },
   };
   return &language;
